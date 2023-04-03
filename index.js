4d3d169a8558c613a352ca74db7f542b179ea227
WymanWai <67907411+WymanWai@users.noreply.github.com>
2023-04-03 14:39:28 +0800

    手機顯示修復

--- index.js
+++ index.js
@@ -39,7 +39,8 @@ app.use("/jquery", express.static("./node_modules/jquery/dist/"));
 var Host = "localhost"; //"192.168.208.12"//"192.168.0.14"//"192.168.66.12"//"192.168.1.188"
 var Port = 5438;
 
-var server = app.listen(Port, function (req, res) {//, Host
+var server = app.listen(Port, function (req, res) {
+  //, Host
   //Host
   var host = server.address().address;
   var port = server.address().port;
